260fed52a38e1fca789d656dcdd0d4807f131727
Dirk Chang <dirk@kooiot.com>
2017-12-20 15:23:25 +0800

    for arm openwrt linux gcc build

--- bindings/lua/premake5.lua
+++ bindings/lua/premake5.lua
@@ -9,11 +9,12 @@ project "opcua"
 	targetprefix ""
 	targetdir "bin/%{cfg.buildcfg}"
 
-	includedirs { "/usr/include/lua5.3", ".", "../../" }
+	--includedirs { "/usr/include/lua5.3", ".", "../../" }
+	includedirs { "/home/cch/mycode/skynet/3rd/lua/", ".", "../../" }
 	files { "**.hpp", "**.cpp"}
 
 	-- buildoptions { '-Wno-unknown-warning', '-Wno-unknown-warning-option', '-Wall', '-Wextra', '-Wpedantic', '-pedantic', '-pedantic-errors', '-Wno-noexcept-type', '-std=c++14', '-ftemplate-depth=1024' }
-	buildoptions { '-Wpedantic', '-pedantic', '-pedantic-errors', '-Wno-noexcept-type', '-ftemplate-depth=2048' }
+	buildoptions { '-Wpedantic', '-pedantic', '-pedantic-errors', '-Wno-noexcept-type', '-std=c++14', '-ftemplate-depth=2048' }
 
 	libdirs { "../../bin" }
 	links { "pthread", "open62541" }
---
+++ tools/cmake/Toolchain-arm-openwrt-linux-gcc.cmake
@@ -0,0 +1,6 @@
+# the name of the target operating system
+set(CMAKE_SYSTEM_NAME Linux)
+
+# which compilers to use for C and C++
+set(CMAKE_C_COMPILER arm-openwrt-linux-gcc)
+set(CMAKE_CXX_COMPILER arm-openwrt-linux-g++)
