739cddc46fcdac1ce442bc24a02987a86d7675ac
Dirk Chang <dirk@kooiot.com>
2018-07-16 16:15:53 +0800

    cross compile with openwrt

--- tools/cmake/Toolchain-mips_24kc-openwrt-linux-gcc.cmake
+++ tools/cmake/Toolchain-mips_24kc-openwrt-linux-gcc.cmake
@@ -2,8 +2,8 @@
 set(CMAKE_SYSTEM_NAME Linux)
 
 # which compilers to use for C and C++
-set(CMAKE_C_COMPILER arm-openwrt-linux-gcc)
-set(CMAKE_CXX_COMPILER arm-openwrt-linux-g++)
+set(CMAKE_C_COMPILER mips-openwrt-linux-gcc)
+set(CMAKE_CXX_COMPILER mips-openwrt-linux-g++)
 
 set(MBEDTLS_FOLDER_INCLUDE "$ENV{STAGING_DIR}/usr/include")
 set(MBEDTLS_FOLDER_LIBRARY "$ENV{STAGING_DIR}/usr/lib")
