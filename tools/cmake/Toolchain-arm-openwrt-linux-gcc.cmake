26e86d21ddee31df3a9b5b4833376ac3b87f9412
Dirk Chang <dirk@kooiot.com>
2020-10-16 19:27:04 +0800

    set the rootdir

--- tools/cmake/Toolchain-arm-openwrt-linux-gcc.cmake
+++ tools/cmake/Toolchain-arm-openwrt-linux-gcc.cmake
@@ -7,3 +7,5 @@ set(CMAKE_CXX_COMPILER arm-openwrt-linux-g++)
 
 set(MBEDTLS_FOLDER_INCLUDE "$ENV{STAGING_DIR}/usr/include")
 set(MBEDTLS_FOLDER_LIBRARY "$ENV{STAGING_DIR}/usr/lib")
+set(OPENSSL_ROOT_DIR "$ENV{STAGING_DIR}/usr")
+
